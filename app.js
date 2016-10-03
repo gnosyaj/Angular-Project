aba3ca2c34add8fa082b241b1b647c61458783cc
Jay Song <jcsong.89@gmail.com>
2016-10-03 09:05:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static('../angularjs'));
+
+app.listen(app.get("port"), function () {
+  console.log("server started on" + app.get("port"));
+});
